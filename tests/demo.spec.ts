38173dd952203133580c2d8c030251f1a005ce6d
superlijin <13688192394@163.com>
2023-12-03 08:21:33 +0800

    demo文件及配置信息

---
+++ tests/demo.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('测试登录百度', async ({ page }) => {
+  await page.goto('https://www.baidu.com/');
+  await page.locator('#kw').click();
+  await page.locator('#kw').fill('java');
+  await page.getByRole('button', { name: '百度一下' }).click();
+});
+
