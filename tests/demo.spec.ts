5118c452b700d2194be2d0913dfee46caacd9ca0
superlijin <13688192394@163.com>
2023-12-10 19:26:31 +0800

    提交hao268的研究代码

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -1,13 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('测试登录百度', async ({ page }) => {
+test('百度', async ({ page }) => {
   await page.goto('https://www.baidu.com/');
   await page.locator('#kw').click();
   await page.locator('#kw').fill('java');
-  await page.getByRole('button', { name: '百度一下' }).click();
+  //await page.getByRole('button', { name: '百度一下' }).click();
+  await page.click('#su');
   console.log("打印网页标题：" + await page.title());
   
 });
 
+test('hao268', async ({ page }) => {
+  await page.goto('https://www.hao268.com/');
+  
+  //input 标签定位写法
+  await page.locator('input[name="wd"]').fill('你好呀!');
 
+  
+});
 
