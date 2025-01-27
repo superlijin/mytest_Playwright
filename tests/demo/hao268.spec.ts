f39ff33c27c9442a8677306e71bd2b970a7da13a
superlijin <13688192394@163.com>
2025-01-27 15:36:06 +0800

    代码提交

--- tests/demo/hao268.spec.ts
+++ tests/demo/hao268.spec.ts
@@ -42,7 +42,7 @@ import { test, expect } from '@playwright/test';
         const mail = page.locator('#hao_mail_options');
         //点击-邮箱控件
         await mail.click();
-        //获取-本地配置的邮箱地址
+        //获取-本地配置的邮箱地址  -- 这里循环遍历暂时行不通，只能把数组给遍历出来。这里后面才发现，出发点就错了，这里的把数组遍历出来后，但实际的值是字符串
         for(var i = 0 ; i < MAIL_DATA.length ; i++ ){
             //await expect(mail).toHaveText(MAIL_DATA[i]);  //不得行
             //console.log('打印：' + MAIL_DATA[i]);
@@ -54,38 +54,18 @@ import { test, expect } from '@playwright/test';
         //console.log('打印邮箱控件内容' + await mail.innerText());
         //console.log('打印设置邮箱控件内容：\n' + MAIL_DATA2);
 
-        //核对-邮箱地址
+        //核对-邮箱地址  --- 这种写法，核对的是 数组，但是实际接收的是一串字符串，所以在这里不适用
         // await expect(mail).toHaveText([
         //     MAIL_DATA[0],
         //     MAIL_DATA[1],
-        //     MAIL_DATA[2],
-        //     MAIL_DATA[3],
-        //     MAIL_DATA[4],
-        //     MAIL_DATA[5],
-        //     MAIL_DATA[6],
-        //     MAIL_DATA[7],
-        //     MAIL_DATA[8],
-        //     MAIL_DATA[9],
-        //     MAIL_DATA[10],
-        //     MAIL_DATA[11],
-        //     MAIL_DATA[12],
-        //     MAIL_DATA[13],
-        //     MAIL_DATA[14],
-        //     MAIL_DATA[15],
-        //     MAIL_DATA[16],
-        //     MAIL_DATA[17],
-        //     MAIL_DATA[18],
-        //     MAIL_DATA[19],
-        //     MAIL_DATA[20],
-        //     MAIL_DATA[21],
-        //     MAIL_DATA[22]
+        //     MAIL_DATA[2]
         // ]);
 
-        //await expect(await mail.innerText()).toMatch(MAIL_DATA[1]);   //可行，也可用于匹配正则表达式
-        //await expect(await mail.innerText()).toEqual([ MAIL_DATA[1],MAIL_DATA[2] ]);  //匹配的方式是数组
-        await expect(await mail.innerText()).toEqual(MAIL_DATA2);   //就用这个了，完美实现
+    //可行方案如下：
+    //await expect(await mail.innerText()).toMatch(MAIL_DATA[1]);   //可行，也可用于匹配正则表达式
+    //await expect(await mail.innerText()).toEqual([ MAIL_DATA[1],MAIL_DATA[2] ]);  //匹配的方式是数组
+    await expect(await mail.innerText()).toEqual(MAIL_DATA2);   //就用这个了，完美实现
     
-        
     });
 
 
